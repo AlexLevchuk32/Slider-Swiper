831e8ea9492d64fda2b4313f6ea9bdf6b092004c
Alex Levchuk <alexlevchuk@mail.ru>
2023-08-05 20:20:10 +0300

    added parallax effect

--- src/js/main.js
+++ src/js/main.js
@@ -3,18 +3,23 @@ import Swiper from 'swiper/bundle';
 
 const swiper = new Swiper('.swiper', {
 	// Optional parameters
-	// direction: 'vertical',
 	loop: true,
+	parallax: true,
+	speed: 1000,
+	keyboard: {
+		enable: true,
+	},
 
 	// If we need pagination
 	pagination: {
-		el: '.swiper-pagination',
+		el: '.slider-controls__count',
+		type: 'fraction',
 	},
 
 	// Navigation arrows
 	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
+		nextEl: '#sliderNext',
+		prevEl: '#sliderPrev',
 	},
 
 	// And if we need scrollbar
